c62a7f2fb399d5d0fbf594b37eea6e4ff6098efa
zhuangbility111 <549694389@qq.com>
2022-05-23 11:21:53 +0900

    update some scripts to profile the performance with different threads.

---
+++ self_benchmark/test_gat_training.sh
@@ -0,0 +1,5 @@
+OMP_NUM_THREADS=1  numactl -C 12 -m 4 python gat_training.py
+OMP_NUM_THREADS=12 numactl -C 12-23 -m 4 python gat_training.py
+OMP_NUM_THREADS=24 numactl -C 12-35 -m 4-5 python gat_training.py
+OMP_NUM_THREADS=36 numactl -C 12-47 -m 4-6 python gat_training.py
+OMP_NUM_THREADS=48 numactl -C 12-59 -m 4-7 python gat_training.py
