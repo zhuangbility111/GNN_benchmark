40d7abbc0124fdfb6d5e4fab864920507fbefcc2
zhuangbility111 <549694389@qq.com>
2022-07-13 22:41:47 +0800

    fix a deadly bug in gcn_training

--- self_benchmark/a64fx/gcn_training.py
+++ self_benchmark/a64fx/gcn_training.py
@@ -45,10 +45,10 @@ print('Number of nodes:', dataset.data.x.shape[0])
 class GCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        # self.conv1 = GCNConv(dataset.num_node_features, 16)
-        # self.conv2 = GCNConv(16, dataset.num_classes)
-        self.conv1 = GCNConv(dataset.num_node_features, dataset.num_node_features)
-        self.conv2 = GCNConv(dataset.num_node_features, dataset.num_classes)
+        self.conv1 = GCNConv(dataset.num_node_features, 16)
+        self.conv2 = GCNConv(16, dataset.num_classes)
+        # self.conv1 = GCNConv(dataset.num_node_features, dataset.num_node_features)
+        # self.conv2 = GCNConv(dataset.num_node_features, dataset.num_classes)
 
     # forward() for tensor
     def forward(self, x, edge):
