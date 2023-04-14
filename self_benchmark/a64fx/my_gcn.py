84112b14a70508a0da3d9d66b5d594fdcddfcd0f
zhuangbility111 <549694389@qq.com>
2023-04-14 17:02:08 +0900

    rm some unnecessary files.

--- self_benchmark/a64fx/my_gcn.py
+++
@@ -1,9 +0,0 @@
-import torch 
-from torch_geometric.data import Data
-from torch_geometric.utils import add_self_loops, degree
-x = torch.tensor([[2,1],[5,6],[3,7],[12,0]],dtype=torch.float)
-y = torch.tensor([0,1,0,1],dtype=torch.long)
-edge_index = torch.tensor([[0,1,1,2,2,3,1,3],[1,0,2,1,3,2,3,1]],dtype=torch.long)
-data = Data(x=x,y=y,edge_index=edge_index)
-
-adjacency_matrix, temp = add_self_loops(edge_index, num_nodes=x.size(0)
