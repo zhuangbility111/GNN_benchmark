78524cfbcf0565b4f017e0785d1a3a8c0b7a0ad9
zhuangbility111 <549694389@qq.com>
2023-04-19 17:34:52 +0900

    fix some bugs in postprocessing.

--- self_benchmark/a64fx/graph_partition_scripts/postprocess_graph_multi_proc.py
+++ self_benchmark/a64fx/graph_partition_scripts/postprocess_graph_multi_proc.py
@@ -48,69 +48,69 @@ def divide_edges_into_local_and_remote(edges_list, node_idx_begin, node_idx_end)
     return local_edges_list, remote_edges_list
 
 def split_nodes_feats(dir_path, graph_name, begin_part, end_part, len_feature):
-        # node_range_on_each_part = []
-        # node_range_on_each_part.append(0)
-        for i in range(begin_part, end_part):
-                # node_id_list = np.loadtxt(os.path.join(dir_path, "p{:0>3d}-{}_nodes.txt".format(i, graph_name)), dtype='int64', delimiter = ' ', usecols=(0, 3))
-                node_id_list = pd.read_csv(os.path.join("./", "p{:0>3d}-{}_nodes.txt".format(i, graph_name)), sep=" ", header=None, usecols=[0, 3]).values
-                # save the node_id_list to npy file
-                np.save(os.path.join(dir_path, "p{:0>3d}-{}_nodes.npy".format(i, graph_name)), node_id_list)
-                begin_idx = node_id_list[0][0]
-                num_nodes = node_id_list.shape[0]
-                end_idx = node_id_list[num_nodes-1][0]
-                # node_range_on_each_part.append(node_range_on_each_part[i] + num_nodes)
-                print(node_id_list.shape)
-                # node_id_list.sort()
-                node_id_list = node_id_list[node_id_list[:, 1].argsort()]
-                # node_feat_list = []
-                node_feat_list = np.empty([num_nodes, len_feature], dtype=np.float32)
-                print(node_feat_list.dtype)
-                idx_in_node_list = 0
-                with open(os.path.join("./", "{}_nodes_feat.txt".format(graph_name))) as file:
-                        for idx, line in enumerate(file):
-                                if idx_in_node_list == len(node_id_list):
-                                        break
-                                elif idx == node_id_list[idx_in_node_list][1]:
-                                        # node_feat_list.append(line.split(" "))
-                                        node_feat_list[node_id_list[idx_in_node_list][0] - begin_idx] = line.split(" ")
-                                        idx_in_node_list += 1
-                # node_feat_list = np.array(node_feat_list, dtype="float32")
-                print(node_feat_list.shape)
-                np.save(os.path.join(dir_path, "p{:0>3d}-{}_nodes_feat.npy".format(i, graph_name)), node_feat_list)
+    # node_range_on_each_part = []
+    # node_range_on_each_part.append(0)
+    for i in range(begin_part, end_part):
+        # node_id_list = np.loadtxt(os.path.join(dir_path, "p{:0>3d}-{}_nodes.txt".format(i, graph_name)), dtype='int64', delimiter = ' ', usecols=(0, 3))
+        node_id_list = pd.read_csv(os.path.join("./", "p{:0>3d}-{}_nodes.txt".format(i, graph_name)), sep=" ", header=None, usecols=[0, 3], dtype='int64').values
+        # save the node_id_list to npy file
+        np.save(os.path.join(dir_path, "p{:0>3d}-{}_nodes.npy".format(i, graph_name)), node_id_list)
+        begin_idx = node_id_list[0][0]
+        num_nodes = node_id_list.shape[0]
+        end_idx = node_id_list[num_nodes-1][0]
+        # node_range_on_each_part.append(node_range_on_each_part[i] + num_nodes)
+        print(node_id_list.shape)
+        # node_id_list.sort()
+        node_id_list = node_id_list[node_id_list[:, 1].argsort()]
+        # node_feat_list = []
+        node_feat_list = np.empty([num_nodes, len_feature], dtype=np.float32)
+        print(node_feat_list.dtype)
+        idx_in_node_list = 0
+        with open(os.path.join("./", "{}_nodes_feat.txt".format(graph_name))) as file:
+            for idx, line in enumerate(file):
+                if idx_in_node_list == len(node_id_list):
+                    break
+                elif idx == node_id_list[idx_in_node_list][1]:
+                    # node_feat_list.append(line.split(" "))
+                    node_feat_list[node_id_list[idx_in_node_list][0] - begin_idx] = line.split(" ")
+                    idx_in_node_list += 1
+        # node_feat_list = np.array(node_feat_list, dtype="float32")
+        print(node_feat_list.shape)
+        np.save(os.path.join(dir_path, "p{:0>3d}-{}_nodes_feat.npy".format(i, graph_name)), node_feat_list)
 
-                # node_label_list = []
-                node_label_list = np.empty([num_nodes], dtype=np.int64)
-                idx_in_node_list = 0
-                with open(os.path.join("./", "{}_nodes_label.txt".format(graph_name))) as file:
-                        for idx, line in enumerate(file):
-                                if idx_in_node_list == len(node_id_list):
-                                        break
-                                elif idx == node_id_list[idx_in_node_list][1]:
-                                        # node_label_list.append(line.split(" "))
-                                        node_label_list[node_id_list[idx_in_node_list][0] - begin_idx] = line.split(" ")[0]
-                                        idx_in_node_list += 1
-                # node_label_list = np.array(node_label_list, dtype="int")
-                print(node_label_list.shape)
-                np.save(os.path.join(dir_path, "p{:0>3d}-{}_nodes_label.npy".format(i, graph_name)), node_label_list)
-                # np.savetxt(os.path.join(dir_path, "part{}".format(i), "nodes_id.txt"), node_id_list, delimiter = ' ')
+        # node_label_list = []
+        node_label_list = np.empty([num_nodes], dtype=np.int64)
+        idx_in_node_list = 0
+        with open(os.path.join("./", "{}_nodes_label.txt".format(graph_name))) as file:
+            for idx, line in enumerate(file):
+                if idx_in_node_list == len(node_id_list):
+                    break
+                elif idx == node_id_list[idx_in_node_list][1]:
+                    # node_label_list.append(line.split(" "))
+                    node_label_list[node_id_list[idx_in_node_list][0] - begin_idx] = line.split(" ")[0]
+                    idx_in_node_list += 1
+        # node_label_list = np.array(node_label_list, dtype="int")
+        print(node_label_list.shape)
+        np.save(os.path.join(dir_path, "p{:0>3d}-{}_nodes_label.npy".format(i, graph_name)), node_label_list)
+        # np.savetxt(os.path.join(dir_path, "part{}".format(i), "nodes_id.txt"), node_id_list, delimiter = ' ')
 
-                # edges_list = np.load(os.path.join(dir_path, "p{:0>3d}-{}_edges.npy".format(i, graph_name)))
-                edges_list = pd.read_csv(os.path.join("./", "p{:0>3d}-{}_edges.txt".format(i, graph_name)), sep=" ", header=None, usecols=[0, 1]).values
-                local_edges_list, remote_edges_list = divide_edges_into_local_and_remote(edges_list, begin_idx, end_idx)
-                print(local_edges_list)
-                print(remote_edges_list)
-                np.save(os.path.join(dir_path, "p{:0>3d}-{}_local_edges.npy".format(i, graph_name)), local_edges_list)
-                np.save(os.path.join(dir_path, "p{:0>3d}-{}_remote_edges.npy".format(i, graph_name)), remote_edges_list)
-        
-        # node_range_on_each_part = np.array(node_range_on_each_part).reshape(1, -1)
-        # np.savetxt(os.path.join(dir_path, "begin_node_on_each_partition.txt"), node_range_on_each_part, fmt = "%d", delimiter = ' ')
+        # edges_list = np.load(os.path.join(dir_path, "p{:0>3d}-{}_edges.npy".format(i, graph_name)))
+        edges_list = pd.read_csv(os.path.join("./", "p{:0>3d}-{}_edges.txt".format(i, graph_name)), sep=" ", header=None, usecols=[0, 1], dtype='int64').values
+        local_edges_list, remote_edges_list = divide_edges_into_local_and_remote(edges_list, begin_idx, end_idx)
+        print(local_edges_list)
+        print(remote_edges_list)
+        np.save(os.path.join(dir_path, "p{:0>3d}-{}_local_edges.npy".format(i, graph_name)), local_edges_list)
+        np.save(os.path.join(dir_path, "p{:0>3d}-{}_remote_edges.npy".format(i, graph_name)), remote_edges_list)
+
+    # node_range_on_each_part = np.array(node_range_on_each_part).reshape(1, -1)
+    # np.savetxt(os.path.join(dir_path, "begin_node_on_each_partition.txt"), node_range_on_each_part, fmt = "%d", delimiter = ' ')
 
 def split_nodes_feats_v1(dir_path, graph_name, begin_part, end_part, len_feature):
     # node_range_on_each_part = []
     # node_range_on_each_part.append(0)
     for i in range(begin_part, end_part):
         # node_id_list = np.loadtxt(os.path.join(dir_path, "p{:0>3d}-{}_nodes.txt".format(i, graph_name)), dtype='int64', delimiter = ' ', usecols=(0, 3))
-        node_id_list = pd.read_csv(os.path.join("./", "p{:0>3d}-{}_nodes.txt".format(i, graph_name)), sep=" ", header=None, usecols=[0, 3]).values
+        node_id_list = pd.read_csv(os.path.join("./", "p{:0>3d}-{}_nodes.txt".format(i, graph_name)), sep=" ", header=None, usecols=[0, 3], dtype='int64').values
         # save the node_id_list to npy file
         np.save(os.path.join(dir_path, "p{:0>3d}-{}_nodes.npy".format(i, graph_name)), node_id_list)
         begin_idx = node_id_list[0][0]
@@ -127,7 +127,7 @@ def split_nodes_feats_v1(dir_path, graph_name, begin_part, end_part, len_feature
         np.save(os.path.join(dir_path, "p{:0>3d}-{}_nodes_label.npy".format(i, graph_name)), local_node_label_list)
         print(local_node_label_list.shape)
 
-        edges_list = pd.read_csv(os.path.join("./", "p{:0>3d}-{}_edges.txt".format(i, graph_name)), sep=" ", header=None, usecols=[0, 1]).values
+        edges_list = pd.read_csv(os.path.join("./", "p{:0>3d}-{}_edges.txt".format(i, graph_name)), sep=" ", header=None, usecols=[0, 1], dtype='int64').values
         local_edges_list, remote_edges_list = divide_edges_into_local_and_remote(edges_list, begin_idx, end_idx)
         print(local_edges_list)
         print(remote_edges_list)
@@ -157,9 +157,12 @@ def compare_array(train_idx, nodes_id_list, node_idx_begin):
 
 def split_node_datamask(dir_path, graph_name, begin_part, end_part):
     remap_start = time.perf_counter()
-    train_idx = pd.read_csv(os.path.join("./", "{}_nodes_train_idx.txt".format(graph_name)), sep=" ", header=None).values
-    valid_idx = pd.read_csv(os.path.join("./", "{}_nodes_valid_idx.txt".format(graph_name)), sep=" ", header=None).values
-    test_idx = pd.read_csv(os.path.join("./", "{}_nodes_test_idx.txt".format(graph_name)), sep=" ", header=None).values
+    # train_idx = pd.read_csv(os.path.join("./", "{}_nodes_train_idx.txt".format(graph_name)), sep=" ", header=None).values
+    # valid_idx = pd.read_csv(os.path.join("./", "{}_nodes_valid_idx.txt".format(graph_name)), sep=" ", header=None).values
+    # test_idx = pd.read_csv(os.path.join("./", "{}_nodes_test_idx.txt".format(graph_name)), sep=" ", header=None).values
+    train_idx = np.load(os.path.join("./", "{}_nodes_train_idx.npy".format(graph_name)))
+    valid_idx = np.load(os.path.join("./", "{}_nodes_valid_idx.npy".format(graph_name)))
+    test_idx = np.load(os.path.join("./", "{}_nodes_test_idx.npy".format(graph_name)))
     for i in range(begin_part, end_part):
         # nodes_id_list = pd.read_csv(os.path.join("./", "p{:0>3d}-{}_nodes.txt".format(i, graph_name)), sep=" ", header=None, usecols=[0, 3]).values
         nodes_id_list = np.load(os.path.join(dir_path, "p{:0>3d}-{}_nodes.npy".format(i, graph_name)))
@@ -174,7 +177,7 @@ def split_node_datamask(dir_path, graph_name, begin_part, end_part):
 
         # remap test mask
         local_test_idx = compare_array(test_idx, nodes_id_list, node_idx_begin)
-                
+
         # np.savetxt(os.path.join(dir_path, "p{:0>3d}-{}_nodes_train_idx.txt".format(i, graph_name)), local_train_idx, fmt = "%d", delimiter = ' ')
         np.save(os.path.join(dir_path, "p{:0>3d}-{}_nodes_train_idx.npy".format(i, graph_name)), local_train_idx)
         # np.savetxt(os.path.join(dir_path, "p{:0>3d}-{}_nodes_valid_idx.txt".format(i, graph_name)), local_valid_idx, fmt = "%d", delimiter = ' ')
@@ -184,41 +187,48 @@ def split_node_datamask(dir_path, graph_name, begin_part, end_part):
 
     remap_end = time.perf_counter()
     print("elapsed time of ramapping dataset mask(ms) = {}".format((remap_end - remap_start) * 1000))
-    
+
 def combined_func(dir_path, graph_name, begin_part, end_part, len_feature):
-        # split_nodes_feats(dir_path, graph_name, begin_part, end_part, len_feature)
-        split_nodes_feats_v1(dir_path, graph_name, begin_part, end_part, len_feature)
-        split_node_datamask(dir_path, graph_name, begin_part, end_part)
+    # split_nodes_feats(dir_path, graph_name, begin_part, end_part, len_feature)
+    split_nodes_feats_v1(dir_path, graph_name, begin_part, end_part, len_feature)
+    split_node_datamask(dir_path, graph_name, begin_part, end_part)
 
 if __name__ == "__main__":
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-d', '--dir_path', type=str, default='./', help='The path of father directory.')
-        parser.add_argument('-g', '--graph_name', type=str, help='The name of graph.')
-        parser.add_argument('-n', '--num_partition', type=int, help='The number of partitioning.')
-        parser.add_argument('-l', '--len_feature', type=int, help='The length of feature vector.')
-        args = parser.parse_args()
-        dir_path = args.dir_path
-        graph_name = args.graph_name
-        num_partition = args.num_partition
-        len_feature = args.len_feature
-        num_process = 16
-        begin_part = 0
-        end_part = num_partition
-        step = int((end_part - begin_part + num_process - 1) / num_process)
-        process_list = []
-        for pid in range(num_process):
-            p = Process(target=combined_func, 
-                        args=(dir_path, graph_name, begin_part + pid*step, min((begin_part + (pid+1)*step), end_part), len_feature))
-            p.start()
-            process_list.append(p)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dir_path', type=str, default='./', help='The path of father directory.')
+    parser.add_argument('-g', '--graph_name', type=str, help='The name of graph.')
+    parser.add_argument('-b', '--begin_partition', type=int, help='The id of beginning partition.')
+    parser.add_argument('-e', '--end_partition', type=int, help='The id of ending partition.')
+    # parser.add_argument('-n', '--num_partition', type=int, help='The number of partitioning.')
+    parser.add_argument('-l', '--len_feature', type=int, help='The length of feature vector.')
+    args = parser.parse_args()
+    dir_path = args.dir_path
+    graph_name = args.graph_name
+    # num_partition = args.num_partition
+    len_feature = args.len_feature
+    num_process = 16
+    begin_part = args.begin_partition
+    end_part = args.end_partition
+    num_partition = end_part - begin_part
+    # begin_part = 0
+    # end_part = num_partition
+    print("begin_part = {}, end_part = {}".format(begin_part, end_part))
+    step = int((end_part - begin_part + num_process - 1) / num_process)
+    process_list = []
 
-        for pid in range(num_process):
-            p.join()
+    for pid in range(num_process):
+        p = Process(target=combined_func,
+                    args=(dir_path, graph_name, begin_part + pid*step, min((begin_part + (pid+1)*step), end_part), len_feature))
+        p.start()
+        process_list.append(p)
 
-        print("All process over!!!")
-        node_range_on_each_part = np.zeros(num_partition+1, dtype=np.int64)
-        for i in range(num_partition): 
-            tmp = np.load(os.path.join(dir_path, "p{:0>3d}-{}_nodes.npy".format(i, graph_name)))
-            node_range_on_each_part[i+1] = tmp[-1][0] + 1
-        np.savetxt(os.path.join(dir_path, "begin_node_on_each_partition.txt"), node_range_on_each_part.reshape(1, -1), fmt = "%d", delimiter = ' ')
+    for p in process_list:
+        p.join()
 
+    print("All process over!!!")
+
+    node_range_on_each_part = np.zeros(num_partition+1, dtype=np.int64)
+    for i in range(num_partition):
+        tmp = np.load(os.path.join(dir_path, "p{:0>3d}-{}_nodes.npy".format(i, graph_name)))
+        node_range_on_each_part[i+1] = tmp[-1][0] + 1
+    np.savetxt(os.path.join(dir_path, "begin_node_on_each_partition.txt"), node_range_on_each_part.reshape(1, -1), fmt = "%d", delimiter = ' ')
