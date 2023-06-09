c0388cf3e54bc9e536b9e0a38734e5016c97c34e
zhuangbility111 <549694389@qq.com>
2023-06-09 11:58:19 +0900

    add submit scripts at abci.

--- self_benchmark/a64fx/sub_scripts_at_abci/run.sh
+++ self_benchmark/a64fx/sub_scripts_at_abci/run.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-#$-l rt_F=16
+#$-l rt_F=2
 #$-cwd
-#$-l h_rt=00:10:00
+#$-l h_rt=00:15:00
 
 # rt_f is the maximum number of nodes, that want to use
 # h_rt is the maximum time, that our module will run at 
 #         the run time of our module should be less than this value#         the format h_rt is Hour:Minute:Second
 
-source ~/gcn.work/dgl_intel_setting_1/env.sh
+# source ~/gcn.work/dgl_intel_setting_1/env.sh
+source ~/gcn.work/dgl_intel_setting_1/env_torch_1.10.0.sh
 source /etc/profile.d/modules.sh
 # source ~/gcn.work/dgl_intel_setting_1/sub407/miniconda3/bin/activate sub407
 
@@ -16,6 +17,10 @@ source /etc/profile.d/modules.sh
 module load intel-mpi/2021.8
 # module load intel-mkl/2023.0.0
 export FI_PROVIDER=tcp
+random_seed=0
+is_fp16=true
+num_nodes=2
+graph_name="products"
 
 # mpi run
 # MPIRUN=mpiexec
@@ -32,4 +37,4 @@ export FI_PROVIDER=tcp
 # cmd="$MPIRUN -np $NP -ppn $NPP ./bin/bioMpi $ImgJ_path"
 # echo run_mu_command: $cmd
 # eval $cmd
-sh run_dist.sh -n 16 -ppn 1 python dist_pyg_test.py --graph_name=products --model=sage --is_async=false --input_dir=./dataset/ogbn_products_new/ogbn_products_16_part
+sh run_dist.sh -n ${num_nodes} -ppn 1 ../python dist_pyg_test.py --graph_name=${graph_name} --model=sage --is_async=false --input_dir=../dataset/ogbn_${graph_name}_new/ogbn_${graph_name}_${num_nodes}_part --random_seed=${random_seed} --is_fp16=${is_fp16} --num_epochs=20
