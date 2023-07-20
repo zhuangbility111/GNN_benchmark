36d6856f6b3a2c9b6da80bb0ac4c9cf08487e5d0
zhuangbility111 <549694389@qq.com>
2023-07-20 17:31:17 +0900

    using the algorithm of finding connected component to shrink the world_size of some subgraphs.

--- self_benchmark/a64fx/test_group_subgraph/KMeans.py
+++ self_benchmark/a64fx/test_group_subgraph/KMeans.py
@@ -3,6 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import silhouette_score
 
+# import for finding connected components
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import connected_components
+
+
 def get_X(data):
     n = int(data.shape[0])
     # collect the upper triangle of data comm matrix as input X for cluster algorithm
@@ -64,10 +69,53 @@ def cluster_for_result(X, cluster_num):
     
     return labels
 
-def save_labels(labels, data):
-    n = int(data.shape[0])
+def find_connected_components(label_matrix):
+    max_label = np.max(label_matrix)
+    new_global_rank_to_labels = []
+    cluster_dict = dict()
+    label_idx_begin = 0
+    for i in range(max_label + 1):
+        # convert the subgraph to a sparse matrix
+        row_idx, col_idx = np.where(label_matrix == i)
+        global_rank = np.unique(np.concatenate([row_idx, col_idx], axis=0))
+        global_rank_to_local_rank = np.zeros(np.max(global_rank) + 1)
+        for k in range(global_rank.shape[0]):
+            global_rank_to_local_rank[global_rank[k]] = k
+        
+        row_idx = global_rank_to_local_rank[row_idx]
+        col_idx = global_rank_to_local_rank[col_idx]
+        values = np.ones_like(row_idx, dtype=np.int32)
+
+        subgraph = csr_matrix((values, (row_idx, col_idx)), shape=(global_rank.shape[0], global_rank.shape[0]))
+        # find the connected components in the subgraph
+        n_components, labels = connected_components(csgraph=subgraph, directed=True, connection='weak', return_labels=True)
+        print("cluster {} has {} connected components".format(i, n_components))
+        print("global_rank = {}".format(global_rank))
+        print("connected components: ", labels)
+
+        labels += label_idx_begin
+        label_idx_begin += n_components
+
+        tmp = dict()
+        for i in range(labels.shape[0]):
+            tmp[global_rank[i]] = labels[i]
+        new_global_rank_to_labels.append(tmp)
+
+        unique_labels = np.unique(labels)
+        for label in unique_labels:
+            cluster_dict[label] = global_rank[labels == label]
+
+    for i in range(label_matrix.shape[0]):
+        for j in range(label_matrix.shape[1]):
+            if label_matrix[i, j] != -1:
+                label_matrix[i, j] = new_global_rank_to_labels[int(label_matrix[i, j])][i]
+    
+    return label_matrix, cluster_dict
+
+def save_labels(label_list, data_volume_matrix):
+    n = int(data_volume_matrix.shape[0])
     k = 0
-    cluster_dict = {}
+    label_matrix = np.zeros_like(data_volume_matrix)
     # recover the cluster result (the upper triangle of data comm matrix) to original data comm matrix
     for i in range(n):
         for j in range(i, n):
@@ -75,24 +123,22 @@ def save_labels(labels, data):
             # so we need to remove these edges from cluster 0 as these edges (volume = 0) don't exist in the original graph
             # if data[i, j] == 0 and data[j, i] == 0, means that there is no edge between vertex i and vertex j
             # so i and j are not in the same communicator
-            if data[i, j] == 0 and data[j, i] == 0:
-                data[i, j] = -1
-                data[j, i] = -1
+            if data_volume_matrix[i, j] == 0 and data_volume_matrix[j, i] == 0:
+                label_matrix[i, j] = -1
+                label_matrix[j, i] = -1
             # but if data[i, j] != 0 or data[j, i] != 0, means that i and j need to be the same communicator for communication
             else:
-                data[i, j] = labels[k]
-                data[j, i] = labels[k]
-                if labels[k] not in cluster_dict:
-                    cluster_dict[labels[k]] = set()
-                cluster_dict[labels[k]].add(i)
-                cluster_dict[labels[k]].add(j)
+                label_matrix[i, j] = label_list[k]
+                label_matrix[j, i] = label_list[k]
             k += 1
+
+    label_matrix, cluster_dict= find_connected_components(label_matrix)
 
     print("cluster_dict: ", cluster_dict)
     for key in cluster_dict:
         print("cluster {} has {} processes".format(key, len(cluster_dict[key])))
 
-    np.save('global_comm_pattern_512proc_clustered_label.npy', data)
+    np.save('global_comm_pattern_512proc_clustered_label.npy', label_matrix)
 
     with open('ranks_list_in_each_clusters_512proc.txt', 'w') as f:
         num_keys = len(cluster_dict.keys())
@@ -110,7 +156,7 @@ if __name__ == "__main__":
     data = np.load('global_comm_pattern_512proc.npy')
     X = get_X(data)
 
-    labels = cluster_for_result(X, 20)
+    labels = cluster_for_result(X, 5)
 
     save_labels(labels, data)
 
