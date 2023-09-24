2d129bca6e1ec6ca8b795dee459afa38950df3c2
zhuangbility111 <549694389@qq.com>
2023-09-24 16:18:56 +0900

    update sub job scripts on abci.

--- self_benchmark/a64fx/sub_scripts_at_abci/batch_job_submission/generate_run_scripts.py
+++ self_benchmark/a64fx/sub_scripts_at_abci/batch_job_submission/generate_run_scripts.py
@@ -1,12 +1,14 @@
 import os
+import argparse
 
-def generate_job_submission_script(num_nodes, num_processes_per_node=2):
+
+def generate_job_submission_script(num_nodes, graph_name, num_processes_per_node=2):
     script = "#!/bin/bash\n"
     script += "\n"
     script += "#$-l rt_F={}\n".format(num_nodes)
     script += "#$-cwd\n"
-    script += "#$-l h_rt=00:15:00\n"
-    script += "\n\n"
+    script += "#$-l h_rt=02:00:00\n"
+    script += "\n"
 
     script += "source ~/gcn.work/dgl_intel_setting_1/env_torch_1.10.0.sh\n"
     script += "source /etc/profile.d/modules.sh\n"
@@ -14,11 +16,6 @@ def generate_job_submission_script(num_nodes, num_processes_per_node=2):
     script += "# load mpi library\n"
     script += "module load intel-mpi/2021.8\n"
     script += "export FI_PROVIDER=tcp\n"
-    script += "random_seed=0\n"
-    script += "is_fp16=true\n"
-    script += "is_async=true\n"
-    script += "graph_name=products\n"
-    script += "num_epochs=10\n"
     script += "\n"
 
     script += "# number of total processes \n"
@@ -28,29 +25,53 @@ def generate_job_submission_script(num_nodes, num_processes_per_node=2):
     script += "# number of processes per node\n"
     script += "NPP={}\n".format(num_processes_per_node)
     script += "\n"
-    
-    params = "--graph_name=${graph_name} --model=sage --is_async=${is_async} --input_dir=../../dataset/ogbn_${graph_name}_new/ogbn_${graph_name}_${NP}_part --random_seed=${random_seed} --is_fp16=${is_fp16} --num_epochs=${num_epochs}"
+
     script += "tcmalloc_path=/home/aaa10008ku/gcn.work/dgl_intel_setting_1/sub407/miniconda3/envs/torch-1.10/lib/libtcmalloc.so\n"
-    script += "LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../../dist_pyg_test.py {}\n".format(params)
-    script += "LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../../dist_pyg_test_pre_post_1_comm.py {}\n".format(params)
+    script += "LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../../train.py --config=../../config/abci/{}.yaml --num_bits=32\n".format(
+        graph_name
+    )
+    script += "LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../../train.py --config=../../config/abci/{}.yaml --num_bits=32 --is_pre_delay=true\n".format(
+        graph_name
+    )
+    # script += "LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../../train.py --config=../../config/abci/{}.yaml --num_bits=-1\n".format(
+    #     graph_name
+    # )
+    # script += "LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../../train.py --config=../../config/abci/{}.yaml --num_bits=-1 --is_pre_delay=true\n".format(
+    #     graph_name
+    # )
+    script += "LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../../train.py --config=../../config/abci/{}.yaml --num_bits=2\n".format(
+        graph_name
+    )
+    script += "LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../../train.py --config=../../config/abci/{}.yaml --num_bits=2 --is_pre_delay=true\n".format(
+        graph_name
+    )
 
     return script
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--graph_name", type=str, default="ogbn-products")
+args = parser.parse_args()
+graph_name = args.graph_name
+
 # Generate job submission scripts for different number of nodes
 num_nodes_list = [1, 2, 4, 8, 16, 32, 64, 128]
+num_processes_per_node = 2
 
 for num_nodes in num_nodes_list:
-    script = generate_job_submission_script(num_nodes)
-    filename = "run_{}.sh".format(num_nodes)
+    script = generate_job_submission_script(num_nodes, graph_name, num_processes_per_node)
+    filename = "run_{}_{}.sh".format(graph_name, num_nodes * num_processes_per_node)
 
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         f.write(script)
 
     print("Generated job submission script: {}".format(filename))
 
 script = "GROUP_ID=gac50544\n"
 for num_nodes in num_nodes_list:
-    script += "qsub -g $GROUP_ID ./run_{}.sh\n".format(num_nodes)
-filename = "submit_all.sh"
-with open(filename, 'w') as f:
+    script += "qsub -g $GROUP_ID -o {}/log/ -e {}/log/ ./run_{}_{}.sh\n".format(
+        graph_name, graph_name, graph_name, num_nodes * num_processes_per_node
+    )
+filename = "submit_{}_all.sh".format(graph_name)
+with open(filename, "w") as f:
     f.write(script)
--- self_benchmark/a64fx/sub_scripts_at_abci/run.sh
+++ self_benchmark/a64fx/sub_scripts_at_abci/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#$-l rt_F=2
+#$-l rt_F=16
 #$-cwd
-#$-l h_rt=00:40:00
+#$-l h_rt=01:00:00
 
 # rt_f is the maximum number of nodes, that want to use
 # h_rt is the maximum time, that our module will run at 
@@ -27,7 +27,7 @@ num_epochs=4
 # MPIRUN=mpiexec
 
 # number of total processes 
-NP=3
+NP=32
 
 # number of processes per node
 NPP=2
@@ -35,4 +35,5 @@ NPP=2
 tcmalloc_path=/home/aaa10008ku/gcn.work/dgl_intel_setting_1/sub407/miniconda3/envs/torch-1.10/lib/libtcmalloc.so
 # LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../dist_pyg_test.py --graph_name=${graph_name} --model=sage --is_async=${is_async} --input_dir=../dataset/ogbn_${graph_name}_new/ogbn_${graph_name}_${NP}_part --random_seed=${random_seed} --is_fp16=${is_fp16} --num_epochs=${num_epochs}
 # LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../dist_pyg_test_pre_post_1_comm.py --graph_name=${graph_name} --model=sage --is_async=${is_async} --input_dir=../dataset/ogbn_${graph_name}_new/ogbn_${graph_name}_${NP}_part --random_seed=${random_seed} --is_fp16=${is_fp16} --num_epochs=${num_epochs}
-LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../train.py --config=../config/abci/ogbn-products.yaml --num_bits=-1
+# LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../train.py --config=../config/abci/ogbn-products.yaml --num_bits=-1 --is_pre_delay=true
+LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD sh run_dist.sh -n $NP -ppn $NPP python ../train.py --config=../config/abci/ogbn-papers100M.yaml --num_bits=2 --is_pre_delay=true
