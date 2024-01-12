070990cefdce26ecb195f7770d0c9ab016336c9e
zhuangbility111 <549694389@qq.com>
2024-01-12 11:17:37 +0900

    move file to play ground.

--- self_benchmark/a64fx/test_half_alltoall/sub_job.sh
+++
@@ -1,2 +0,0 @@
-GROUP_ID=gac50544
-qsub -g $GROUP_ID ./run.sh
--- self_benchmark/a64fx/test_scripts/test_dist_gcn.sh
+++
@@ -1,4 +0,0 @@
-mpirun -np 1 -std output.log python dist_pyg_test.py --tensor_type=sparse_tensor
-mpirun -np 2 -std output.log python dist_pyg_test.py --tensor_type=sparse_tensor
-mpirun -np 4 -std output.log python dist_pyg_test.py --tensor_type=sparse_tensor
-mpirun -np 8 -std output.log python dist_pyg_test.py --tensor_type=sparse_tensor
